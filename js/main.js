a63c9caa289820ab96d2f485b22129a19dc9f05f
Isaac Bilyea <isaacbilyea@gmail.com>
2024-01-31 14:15:35 -0500

    Update main.js
    Switched out the code to use a forEach loop as learned in class.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,9 @@
 console.log("JS file is connected!");
 
-function logThisId() {
-    console.log(this);
+const fruits = document.querySelectorAll('#fruits svg:not(#logo)');
+
+function logId() {
     console.log('This is a', this.id);
 }
 
-//Grapefruit
-const grapefruitGraphic = document.querySelector('#Grapefruit');
-console.log(grapefruitGraphic)
-grapefruitGraphic.addEventListener('click', logThisId);
-
-//Orange
-const orangeGraphic = document.querySelector('#Orange');
-console.log(orangeGraphic)
-orangeGraphic.addEventListener('click', logThisId);
-
-//Pineapple
-const pineappleGraphic = document.querySelector('#Pineapple');
-console.log(pineappleGraphic)
-pineappleGraphic.addEventListener('click', logThisId);
-
-//Lime
-const limeGraphic = document.querySelector('#Lime');
-console.log(limeGraphic)
-limeGraphic.addEventListener('click', logThisId);
-
-//Blueberry
-const blueberryGraphic = document.querySelector('#Blueberry');
-console.log(blueberryGraphic)
-blueberryGraphic.addEventListener('click', logThisId);
-
-//Passion Fruit
-const passionfruitGraphic = document.querySelector('#Passion_Fruit');
-console.log(passionfruitGraphic)
-passionfruitGraphic.addEventListener('click', logThisId);
+fruits.forEach(fruit => fruit.addEventListener('click', logId));
